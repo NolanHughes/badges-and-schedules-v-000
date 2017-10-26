61a8ff2c530d4ee172116a45cd1abfbe14b33f7d
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 19:27:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,9 @@
 # Write your code here.
 def badge_maker(name)
   # badge_name = []
-  # name.each do |speaker|
-  #   badge_name << "Hello, my name is #{speaker}."
-  # end
+  name.each do |speaker|
+    speaker
+    # badge_name << "Hello, my name is #{speaker}."
+  end
   # badge_name
 end
