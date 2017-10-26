9665e2517280e7710db76f2240480798cc00ccc8
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 19:35:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,12 @@
 # Write your code here.
 def badge_maker(name)
   badge_name = "Hello, my name is #{name}."
-  # name.each do |speaker|
-  #   badge_name << "Hello, my name is #{speaker}."
-  # end
   badge_name
+end
+
+def batch_badge_creator(name)
+  badge_name = []
+  name.each do |speaker|
+    badge_name << "Hello, my name is #{speaker}."
+  end
 end
